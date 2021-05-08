bf1e50ed0e0d5671713aa02c5af352309a9dbefb
Moyise <moyisemr@gmail.com>
2021-05-08 02:52:42 -0400

    first commit

---
+++ src/components/layout/Footer02.js
@@ -0,0 +1,111 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Logo from "./partials/Logo";
+import FooterNav from "./partials/FooterNav";
+import FooterSocial from "./partials/FooterSocial";
+
+const propTypes = {
+  topOuterDivider: PropTypes.bool,
+  topDivider: PropTypes.bool,
+};
+
+const defaultProps = {
+  topOuterDivider: false,
+  topDivider: false,
+};
+
+class Footer extends React.Component {
+  render() {
+    const { className, topOuterDivider, topDivider, ...props } = this.props;
+
+    const classes = classNames(
+      "site-footer",
+      topOuterDivider && "has-top-divider",
+      className
+    );
+
+    const date = new Date().getFullYear();
+
+    return (
+      <footer {...props} className={classes}>
+        <div className="container">
+          <div
+            className={classNames("site-footer-inner", topDivider && "has-top-divider")}
+          >
+            <div className="footer-top text-xxs">
+              <div className="footer-blocks">
+                <div className="footer-block">
+                  <Logo className="mb-16" />
+                  <div className="footer-copyright">
+                    &copy; {date} Kane's, all rights reserved
+                  </div>
+                </div>
+                <div className="footer-block">
+                  <div className="footer-block-title">Company</div>
+                  <ul className="list-reset mb-0">
+                    <li>
+                      <a href="https://www.m-kane.com/">Dummy text used</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">The purpose of lorem</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Filler text can be very useful</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Be on design</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="footer-block">
+                  <div className="footer-block-title">Uses cases</div>
+                  <ul className="list-reset mb-0">
+                    <li>
+                      <a href="https://www.m-kane.com/">Consectetur adipiscing</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Lorem Ipsum is place</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Excepteur sint</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Occaecat cupidatat</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="footer-block">
+                  <div className="footer-block-title">Docs</div>
+                  <ul className="list-reset mb-0">
+                    <li>
+                      <a href="https://www.m-kane.com/">The purpose of lorem</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Dummy text used</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Excepteur sint</a>
+                    </li>
+                    <li>
+                      <a href="https://www.m-kane.com/">Occaecat cupidatat</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="footer-bottom space-between center-content-mobile text-xxs">
+              <FooterNav />
+              <FooterSocial />
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+Footer.propTypes = propTypes;
+Footer.defaultProps = defaultProps;
+
+export default Footer;
